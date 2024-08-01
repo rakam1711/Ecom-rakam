edf23ebc3f18a55c2d2b25ee63de8a6d0c636ce6
rakam nagar <rakamsingh1711@gmail.com>
2024-08-01 15:58:16 +0530

    bug fixed

--- src/Modules/user/controller/register.js
+++ src/Modules/user/controller/register.js
@@ -2,41 +2,51 @@ const userSchema = require("../model/userSchema.js");
 const jwt = require("jsonwebtoken");
 const { encrypt } = require("../../../Middleware/encryption.js");
 const register = async (req, res) => {
-    try {
-        const mustData = {
-            fullName: req.body.fullName,
-            number: req.body.number,
-            email: req.body.email,
-            gender: req.body.gender,
-            address: req.body.address
-        };
-        for (let key in mustData) {
-            if (mustData[key] == undefined || mustData[key] == "") {
-                throw new Error(`Invalid feild ${key}`);
-            }
-        }
-
-        const user = await userSchema.findOne({ mobile_number: mustData.number });
-        if (!user) {
-            const addUser = new userSchema({
-                name: mustData.fullName,
-                number: mustData.number,
-                email: mustData.email,
-                gender: mustData.gender,
-                address: mustData.address
-            })
-
-            await addUser.save();
-            const tokenwa = jwt.sign({ id: addUser._id }, process.env.JWTSECRET);
-            const token = await encrypt(tokenwa);
-            return res.status(201).json({ status: true, message: "Created Successfully", token, user: addUser })
-        } else {
-            return res.status(200).json({ status: false, message: "already register user" })
-        }
-
-    } catch (err) {
-        return res.status(500).json({ status: false, message: err.message, location: "src/Modules/use/controller/register" })
+  try {
+    const mustData = {
+      fullName: req.body.fullName,
+      number: req.body.number,
+      email: req.body.email,
+      gender: req.body.gender,
+      address: req.body.address,
+    };
+    for (let key in mustData) {
+      if (mustData[key] == undefined || mustData[key] == "") {
+        throw new Error(`Invalid feild ${key}`);
+      }
     }
-}
+
+    const user = await userSchema.findOne({ number: mustData.number });
+    if (!user) {
+      const addUser = new userSchema({
+        name: mustData.fullName,
+        number: mustData.number,
+        email: mustData.email,
+        gender: mustData.gender,
+        address: mustData.address,
+      });
+
+      await addUser.save();
+      const tokenwa = jwt.sign({ id: addUser._id }, process.env.JWTSECRET);
+      const token = await encrypt(tokenwa);
+      return res.status(201).json({
+        status: true,
+        message: "Created Successfully",
+        token,
+        user: addUser,
+      });
+    } else {
+      return res
+        .status(200)
+        .json({ status: false, message: "already register user" });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      message: err.message,
+      location: "src/Modules/use/controller/register",
+    });
+  }
+};
 
 module.exports = register;
--- src/Modules/user/model/userSchema.js
+++ src/Modules/user/model/userSchema.js
@@ -1,33 +1,37 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        require: true,
+      type: String,
+      require: true,
     },
     email: {
-        type: String,
-        lowercase: true,
-        validate: {
-            validator: function (v) {
-                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
-            },
-            message: "{VALUE} is not a valid email",
+      type: String,
+      lowercase: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
         },
+        message: "{VALUE} is not a valid email",
+      },
     },
     number: {
-        type: Number,
-        required: true,
-        unique: true,
-        validate: {
-            validator: function (v) {
-                return /^[0-9]{10}$/.test(v);
-            },
-            message: "{VALUE} is not a valid mobile number!",
+      type: Number,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^[0-9]{10}$/.test(v);
         },
+        message: "{VALUE} is not a valid mobile number!",
+      },
     },
     gender: { type: String },
-    address: { type: String }
-}, { timestamps: true });
+    address: { type: String },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("user", userSchema);
