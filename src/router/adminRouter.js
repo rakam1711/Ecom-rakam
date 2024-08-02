89db33255067037332c7f96c014ce2e4b229d00c
pharhan7 <farhan@teknikoglobal.in>
2024-08-02 16:55:11 +0530

    asdasd

--- src/router/adminRouter.js
+++ src/router/adminRouter.js
@@ -5,8 +5,12 @@ const authenticateUser = require("../Middleware/JWT/userAuthentication.js");
 const loginAdmin = require("../Modules/admin/controller/loginAdmin");
 const createCategory = require("../Modules/category/controller/createCategory.js");
 const getAllCategory = require("../Modules/category/controller/getAllCategory.js");
+const updateCategory = require("../Modules/category/controller/updateCategory.js");
+const createSubCategory = require("../Modules/subCategory/controller/createSubCategory.js");
 
 adminroutes.post("/adminlogin", loginAdmin);
 adminroutes.post("/createcategory", authenticateUser, createCategory);
+adminroutes.post("/createsubcategory", authenticateUser, createSubCategory);
 adminroutes.post("/listcategory", getAllCategory);
+adminroutes.post("/updatecategory", updateCategory);
 module.exports = adminroutes;
