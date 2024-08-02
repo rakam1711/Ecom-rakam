7d10f540c122d33da180d52540fb1da312d56df7
rakam nagar <rakamsingh1711@gmail.com>
2024-08-02 16:50:51 +0530

    created sub category

--- src/Modules/subCategory/model/subcategorySchema.js
+++ src/Modules/subCategory/model/subcategorySchema.js
@@ -1,32 +1,34 @@
 const mongoose = require("mongoose");
 
-
-const subCategoryModel = new mongoose.Schema({
-
+const subCategoryModel = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+    },
     name: {
-        type: String,
-        unique: true
+      type: String,
+      unique: true,
     },
     description: {
-        type: String,
-        unique: true
+      type: String,
+      unique: true,
     },
     category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "category"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "category",
     },
     is_active: {
-        type: Boolean,
-        default: true,
+      type: Boolean,
+      default: true,
     },
     isDeleted: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-}, { timestamps: true });
-
+  },
+  { timestamps: true }
+);
 
 const subCategorySchema = mongoose.model("subCategory", subCategoryModel);
 
 module.exports = subCategorySchema;
-
