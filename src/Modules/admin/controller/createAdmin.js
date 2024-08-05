e6626eec8bbcbe6a6dad63a7c55ab300e3ecd8c2
rakam <rakamsingh1711@gmail.com>
2024-08-05 15:57:54 +0530

    asdfgre

---
+++ src/Modules/admin/controller/createAdmin.js
@@ -0,0 +1,40 @@
+const Admin = require("../model/adminSchema.js");
+
+const createAdmin = async (req, res, next) => {
+  try {
+    const mustData = {
+      number: req.body.number,
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+    };
+    for (let key in mustData) {
+      if (mustData[key] == undefined || mustData[key] == "") {
+        throw new Error(`invalid field ${key}`);
+      }
+    }
+    const salt = await bcryptjs.genSaltSync(2);
+    const hashedPassword = await bcryptjs.hash(mustData.password, salt);
+
+    const admin = Admin({
+      number: mustData.number,
+      email: mustData.email,
+      name: mustData.name,
+      password: hashedPassword,
+    });
+
+    await admin.save();
+    return res.status(201).json({
+      status: true,
+      message: "admin created successfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      message: err.message,
+      location: "src/Modules/admin/controller/createAdmin",
+    });
+  }
+};
+
+module.exports = createAdmin;
