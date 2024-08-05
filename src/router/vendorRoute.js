fa8e33fdb8cdfa73b8c8749c430902778ae8abe5
rakam <rakamsingh1711@gmail.com>
2024-08-05 12:49:50 +0530

    route added

--- src/router/vendorRoute.js
+++ src/router/vendorRoute.js
@@ -1,13 +1,14 @@
 const vendorRoute = require("express").Router();
 
 // const loginUser = require("../user/controller/loginUser.js")
-const register = require("../Modules/vendor/controller/register.js")
-const findUserName = require("../Modules/vendor/controller/findUserName.js")
-const login = require("../Modules/vendor/controller/login.js")
+const register = require("../Modules/vendor/controller/register.js");
+const findUserName = require("../Modules/vendor/controller/findUserName.js");
+const login = require("../Modules/vendor/controller/login.js");
+const createShop = require("../Modules/shop/controller/createShopController.js");
 
 vendorRoute.post("/register", register);
 vendorRoute.post("/findusername", findUserName);
 vendorRoute.post("/login", login);
-
+vendorRoute.post("/createshop", createShop);
 
 module.exports = vendorRoute;
