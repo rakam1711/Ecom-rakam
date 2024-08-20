a3ee29302d01f30ac6bb06ef151dbb4637d690ba
pharhan7 <farhan@teknikoglobal.in>
2024-08-20 15:02:53 +0530

    Latest

--- src/router/categoryRouter.js
+++
@@ -1,7 +0,0 @@
-const categoryRouter = require("express").Router();
-
-// const createCategory = require("")
-
-// categoryRouter.post("/create", createCategory)
-
-module.exports = categoryRouter;
--- src/router/subCategoryRouter.js
+++
@@ -1,7 +0,0 @@
-const subCategoryRouter = require("express").Router();
-
-// const createCategory = require("")
-
-// subCategoryRouter.post("/create", createCategory)
-
-module.exports = subCategoryRouter;
