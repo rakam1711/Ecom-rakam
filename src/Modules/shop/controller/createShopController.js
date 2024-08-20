dde2755adae7dd505d0fa65eed52ea90ecea2e0f
pharhan7 <farhan@teknikoglobal.in>
2024-08-20 17:49:25 +0530

    Shop Image Fix

--- src/Modules/shop/controller/createShopController.js
+++ src/Modules/shop/controller/createShopController.js
@@ -1,43 +1,53 @@
 const Shop = require("../model/shopSchema.js");
+const upload = require("../../../Middleware/multer/singleImageUpload.js");
+const BASE_URL = process.env.BASE_URL;
 
 const createShop = async (req, res, next) => {
-  try {
-    const mustData = {
-      name: req.body.name,
-      description: req.body.description,
-      address: req.body.address,
-      contactInfo: req.body.contactInfo,
-      categories: req.body.categories,
-    };
-    for (let key in mustData) {
-      if (mustData[key] == undefined || mustData[key] == "") {
-        throw new Error(`invalid field ${key}`);
+  upload(req, res, async () => {
+
+    try {
+      const mustData = {
+        name: req.body.name,
+        description: req.body.description,
+        address: req.body.address,
+        contactInfo: req.body.contactInfo,
+        categories: req.body.categories,
+      };
+      for (let key in mustData) {
+        if (mustData[key] == undefined || mustData[key] == "") {
+          throw new Error(`invalid field ${key}`);
+        }
       }
+
+      if (req.file) {
+        mustData.image = BASE_URL + req.file.path;
+      }
+
+      const shop = Shop({
+        name: mustData.name,
+        description: mustData.description,
+        address: mustData.address,
+        contactInfo: mustData.contactInfo,
+        owner: req.vendorId,
+        subCategories: mustData.categories,
+        logo: mustData.mustData.image,
+      });
+
+      await shop.save();
+      return res.status(201).json({
+        status: true,
+        message: "shop created successfully",
+      });
+    } catch (err) {
+      return res.status(500).json({
+        status: false,
+        message: err.message,
+        location: "src/Modules/shop/controller/createShopController",
+      });
     }
-    // const shopName = await Shop.findOne({ name: mustData.name });
-    // if (shopName) throw new Error("Shop Name already present");
 
-    const shop = Shop({
-      name: mustData.name,
-      description: mustData.description,
-      address: mustData.address,
-      contactInfo: mustData.contactInfo,
-      owner: req.vendorId,
-      subCategories: mustData.categories,
-    });
 
-    await shop.save();
-    return res.status(201).json({
-      status: true,
-      message: "shop created successfully",
-    });
-  } catch (err) {
-    return res.status(500).json({
-      status: false,
-      message: err.message,
-      location: "src/Modules/shop/controller/createShopController",
-    });
-  }
+  })
 };
 
 module.exports = createShop;
--- src/Modules/shop/controller/updateShopController.js
+++ src/Modules/shop/controller/updateShopController.js
@@ -1,32 +1,39 @@
 const Shop = require("../model/shopSchema.js");
-
+const upload = require("../../../Middleware/multer/singleImageUpload.js");
+const BASE_URL = process.env.BASE_URL;
 const updateShop = async (req, res, next) => {
-  try {
-    const data = {
-      name: req.body.name,
-      description: req.body.description,
-      address: req.body.address,
-      contactInfo: req.body.contactInfo,
-    };
-    for (let key in data) {
-      if (data[key] == undefined || data[key] == "") {
-        delete data[key];
+  upload(req, res, async () => {
+    try {
+      const data = {
+        name: req.body.name,
+        description: req.body.description,
+        address: req.body.address,
+        contactInfo: req.body.contactInfo,
+      };
+      if (req.file) {
+        data.logo = BASE_URL + req.file.path;
       }
+      for (let key in data) {
+        if (data[key] == undefined || data[key] == "") {
+          delete data[key];
+        }
+      }
+      await Shop.findOneAndUpdate({ owner: req.vendorId }, data, {
+        new: true,
+      });
+      return res.status(200).json({
+        status: true,
+        message: "shop updated successfully.",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: false,
+        message: error.message,
+        location: "src/Modules/Shop/controller/updateShop",
+      });
     }
-    await Shop.findOneAndUpdate({ owner: req.vendorId }, data, {
-      new: true,
-    });
-    return res.status(200).json({
-      status: true,
-      message: "shop updated successfully.",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      status: false,
-      message: error.message,
-      location: "src/Modules/Shop/controller/updateShop",
-    });
-  }
+  })
+
 };
 
 module.exports = updateShop;
