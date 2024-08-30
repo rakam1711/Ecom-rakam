f604273bc5ea0bba4cbeee35f55eba8644c5a651
rakam <rakamsingh1711@gmail.com>
2024-08-30 15:14:28 +0530

    bugs fixed in list subcategory

--- src/Modules/subCategory/controller/listSubCategory.js
+++ src/Modules/subCategory/controller/listSubCategory.js
@@ -9,7 +9,7 @@ const listSubCategory = async (req, res) => {
 
     if (!categoryId) throw new Error("categoryId is missing");
     const data = await subCategorySchema
-      .find({ category: categoryId })
+      .findById({ _id: categoryId })
       .skip((page - 1) * limit)
       .limit(limit);
     if (!data)
