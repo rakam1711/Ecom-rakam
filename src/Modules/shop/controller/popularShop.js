36ca233b6593f5810ed497a2981c7bdc01065641
pharhan7 <farhan@teknikoglobal.in>
2024-09-20 12:38:24 +0530

    popular shop for all

--- src/Modules/shop/controller/popularShop.js
+++ src/Modules/shop/controller/popularShop.js
@@ -4,14 +4,39 @@ const popularShop = async (req, res) => {
     try {
         const page = req.body.page || 1;
         const limit = req.body.limit || 1000;
+        const service = req.body.serviceId;
+        const category = req.body.categoryId;
         const skip = (page - 1) * limit;
-        const count = Shop.countDocuments({ isPopular: true });
-        const data = await Shop.find({ isPopular: true }).skip(skip).limit(limit);
-        const maxpage = Math.ceil(count) || 0;
-        return res.status(200).json({ status: true, result: data, maxPage: maxpage })
+        
+        const pipeline = await getPipeline(service, category);
+        const data = await Shop.aggregate(pipeline).skip(skip).limit(limit);
+
+        const count = await Shop.countDocuments({ isPopular: true });
+        const maxPage = Math.ceil(count / limit) || 0;
+
+        return res.status(200).json({ status: true, result: data, maxPage });
 
     } catch (err) {
-        return res.status(400).json({ status: false, message: err.message, location: "src/Modules/shop/controller/popularShop" })
+        return res.status(400).json({ status: false, message: err.message, location: "src/Modules/shop/controller/popularShop" });
     }
-}
+};
+
 module.exports = popularShop;
+
+const getPipeline = async (service, category) => {
+    const matchStage = { isPopular: true };
+
+    if (service) {
+        matchStage.service = mongoose.Types.ObjectId(service); 
+    }
+
+    if (category) {
+        matchStage.categories = mongoose.Types.ObjectId(category); 
+    }
+
+    const pipeline = [
+        { $match: matchStage },
+    ];
+
+    return pipeline;
+};
