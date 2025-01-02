957a70924c25bdf2f4b7995621a7b142138a2a72
rakam <rakamsingh1711@gmail.com>
2025-01-02 17:54:35 +0530

    effdml;c

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,19 +10,20 @@ module.exports = scripts;
 async function createFirstAdmin() {
   try {
     const result = await Admin.findOne();
-    if (result) return;
-    const salt = bcryptjs.genSaltSync(2);
-    const hashedPassword = await bcryptjs.hash("1212", salt);
+    if (!result) {
+      const salt = bcryptjs.genSaltSync(2);
+      const hashedPassword = await bcryptjs.hash("1212", salt);
 
-    const admin = new Admin({
-      number: "9310235717",
-      email: "admin@gmail.com",
-      name: "Admin",
-      role:"ADMIN",
-      password: hashedPassword,
-    });
-    await admin.save();
-    console.log("New admin is created");
+      const admin = new Admin({
+        number: "9310235717",
+        email: "admin@gmail.com",
+        name: "Admin",
+        role: "ADMIN",
+        password: hashedPassword,
+      });
+      await admin.save();
+      console.log("New admin is created");
+    }
   } catch (err) {
     console.log(err.message);
   }
