55d4066dabef89dd6e9e49c5a4e1d429763467d0
pharhan7 <developerfarhan7@gmail.com>
2025-01-09 18:28:15 +0530

    lll

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const mongoose = require("mongoose");
 const connectToDatabase = require("./dbconnection/connection.js");
 const app = require("./src/app");
 const scripts = require("./src/scripts/index.js");
+const cluster = require("cluster");
+const os = require("os");
 
 const { PORT, BASE_URL } = process.env;
 
-(async () => {
+if (cluster.isMaster) {
+  const cpus = os.cpus().length;
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.id} exited`);
+    cluster.fork();
+  });
+} else {
+  startServer();
+}
+
+async function startServer() {
   try {
     console.log("Initializing server");
     await connectToDatabase();
@@ -17,7 +32,21 @@ const { PORT, BASE_URL } = process.env;
   } catch (error) {
     shutdown(error);
   }
-})();
+}
+
+
+// (async () => {
+//   try {
+//     console.log("Initializing server");
+//     await connectToDatabase();
+//     await scripts();
+//     app
+//       .listen(PORT, () => console.log(`Server is running on ${BASE_URL}`))
+//       .on("error", shutdown);
+//   } catch (error) {
+//     shutdown(error);
+//   }
+// })();
 
 async function shutdown(err) {
   console.log("Unable to initialize the server:", err.message);
